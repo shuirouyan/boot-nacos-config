5d6194453ebcb0f6c707a59b02b88b30deba18ed
shuirouyan <1141761757@qq.com>
2025-06-27 17:07:18 +0800

    boot3 mcp服务注册到nacos-server,使用mcp-inspector调试tools

--- src/main/java/com/example/nacosproperties/service/WeatherService.java
+++ src/main/java/com/example/nacosproperties/service/WeatherService.java
@@ -1,5 +1,9 @@
 package com.example.nacosproperties.service;
 
+import com.example.nacosproperties.entity.Jokes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
@@ -7,8 +11,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class WeatherService {
 
+  @Resource
+  private JokesService jokesService;
+
   @Tool(description = "Get weather information by city name")
   public String getWeather(@ToolParam(description = "City name") String cityName) {
     return "Sunny in " + cityName;
+  }
+
+  @Tool(description = "获取一个笑话")
+  public String getJokeService(@ToolParam(description = "笑话id") Integer id) {
+    Jokes byId = jokesService.getById(id);
+    if (byId != null) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      try {
+        return objectMapper.writeValueAsString(byId);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return "id is not jokes;";
   }
 }
